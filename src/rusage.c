ae538dca051f5f0f86995f28e1d6b58469c50736
ksqsf <i@ksqsf.moe>
2019-09-02 17:58:09 +0800

    Fix build on Linux

--- src/rusage.c
+++ src/rusage.c
@@ -8,69 +8,44 @@ struct disk_info_t {
 #if defined(__linux__)
 
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define STATIC_BUF_LEN 4096
 
-int proc_disk_info(struct disk_info_t *buf)
+int proc_disk_info(struct disk_info_t *diskbuf)
 {
-     char sbuf[STATIC_BUF_LEN] = { 0 };
-     char *buf = sbuf;
-     int fd = open("/proc/self/io", O_RDONLY);
-     off_t offset;
-     size_t size;
+     char buf[STATIC_BUF_LEN] = { 0 };
+     int fd;
      int ret;
-     int nbytes;
+     ssize_t nbytes;
 
-     // Determine buffer size.
-     offset = lseek(fd, 0, SEEK_END);
-     if (offset == -1) {
+     // Open
+     fd = open("/proc/self/io", O_RDONLY);
+     if (fd < 0) {
           return errno;
-     }
-     size = (size_t) offset;
-
-     // Rewind
-     if (lseek(fd, 0, SEEK_SET) < 0) {
-          return errno;
-     }
-
-     // Allocate buffer dynamically if sbuf is too small
-     if (size > STATIC_BUF_LEN-1) {
-          buf = (char *) malloc(size + 10);
-          if (!buf) {
-               return ENOMEM;
-          }
-          memset(buf, 0, size + 10);
-     } else {
-          memset(sbuf, 0, sizeof(sbuf));
      }
 
      // Read file into buffer
-     nbytes = 0;
-     do {
-          ssize_t once = read(fd, buf + nbytes, size);
-          if (once < 0) {
-               ret = errno;
-               goto finish;
-          }
-          else if (once == 0) {
-               break;
-          }
-          nbytes += once;
-     } while (nbytes < size);
+     nbytes = read(fd, buf, STATIC_BUF_LEN);
+     if (nbytes < 0) {
+          ret = errno;
+          goto finish;
+     }
 
      // Extract information from buffer
-     uint64_t rchar, wchar, syscr, syscw, read_bytes, write_bytes, cancelled_write_bytes;
-     sscanf("rchar: %llu\nwchar: %llu\nsyscr: %llu\nsyscw: %llu\nread_bytes: %llu\nwrite_bytes: %llu\ncancelled_write_bytes: %llu\n", &rchar, &wchar, &syscr, &syscw, &read_bytes, &write_bytes, &cancelled_write_bytes);
-     buf->read_bytes = read_bytes;
-     buf->write_bytes = write_bytes;
+     unsigned long long rchar, wchar, syscr, syscw, read_bytes, write_bytes, cancelled_write_bytes;
+     sscanf(buf, "rchar: %llu\nwchar: %llu\nsyscr: %llu\nsyscw: %llu\nread_bytes: %llu\nwrite_bytes: %llu\ncancelled_write_bytes: %llu\n", &rchar, &wchar, &syscr, &syscw, &read_bytes, &write_bytes, &cancelled_write_bytes);
+     diskbuf->read_bytes = read_bytes;
+     diskbuf->write_bytes = write_bytes;
 
 finish:
-     // Free dynamic buffer.
-     if (buf != sbuf) {
-          free(buf);
-     }
+     close(fd);
      return ret;
 }
 
